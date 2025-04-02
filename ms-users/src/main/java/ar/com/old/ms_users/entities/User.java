aa1ef12dc93488c270b044b2e68a6bc30d98c688
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-04-02 19:33:53 -0300

    feat(User): ms_users - add User entity.

---
+++ ms-users/src/main/java/ar/com/old/ms_users/entities/User.java
@@ -0,0 +1,46 @@
+package ar.com.old.ms_users.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String userName;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    public User() {
+    }
+
+    public User(Long id, String userName, String password, String email) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
