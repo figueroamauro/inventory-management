87453ac5261305e4aaf0a71a5d0c163645c5e63b
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-04-04 23:47:46 -0300

    feat(GlobalExceptionHandler): Implement a global handler to manage exception in controllers

---
+++ ms-users/src/main/java/ar/com/old/ms_users/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package ar.com.old.ms_users.handlers;
+
+import ar.com.old.ms_users.exceptions.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleNotFoundException(Exception ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("error", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+}
