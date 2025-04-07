2ffe9b334b80bbfda65a66ea96bab1b67900eb0d
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-04-07 16:50:55 -0300

    feat(UserRequestDTO): Add validation for username length(4-20 chars)

--- ms-users/src/main/java/ar/com/old/ms_users/dto/UserRequestDTO.java
+++ ms-users/src/main/java/ar/com/old/ms_users/dto/UserRequestDTO.java
@@ -1,11 +1,13 @@
 package ar.com.old.ms_users.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record UserRequestDTO(
         Long id,
 
         @NotBlank(message = "Username can not be blank")
+        @Size(min = 4, max = 20, message = "Username must be between 4 and 20 characters long")
         String userName,
 
         String password,
--- ms-users/src/test/java/ar/com/old/ms_users/dto/UserRequestDTOTest.java
+++ ms-users/src/test/java/ar/com/old/ms_users/dto/UserRequestDTOTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 
 import java.util.Set;
@@ -15,6 +16,11 @@ import static jakarta.validation.Validation.buildDefaultValidatorFactory;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserRequestDTOTest {
+    private static final String NAME_TOO_LONG_20 = "AAAAABBBBBCCCCCDDDDDE";
+    public static final String CORRECT_PASS = "pass123";
+    public static final String CORRECT_EMAIL = "test@mail.com";
+    public static final String CORRECT_USERNAME = "test";
+
     private Validator validator;
     private UserRequestDTO dto;
     private Set<ConstraintViolation<UserRequestDTO>> errors;
@@ -30,7 +36,7 @@ class UserRequestDTOTest {
     @Test
     void shouldCreateDto_whenAllFieldsAreValid() {
         //GIVEN
-        dto = new UserRequestDTO(1L, "test", "pass123", "test@mail.com");
+        dto = new UserRequestDTO(1L, CORRECT_USERNAME, CORRECT_PASS, CORRECT_EMAIL);
 
         //WHEN
         errors = validator.validate(dto);
@@ -43,13 +49,27 @@ class UserRequestDTOTest {
     @NullAndEmptySource
     void shouldThrowException_whenUserNameIsBlank(String username) {
         //GIVEN
-        dto = new UserRequestDTO(1L, username, "pass123", "test@mail.com");
+        dto = new UserRequestDTO(1L, username, CORRECT_PASS, CORRECT_EMAIL);
 
         //WHEN
         errors = validator.validate(dto);
 
         //THEN
         assertTrue(errors.stream().anyMatch(e -> e.getMessage().equals("Username can not be blank")));
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"joh", NAME_TOO_LONG_20})
+    void shouldThrowException_whenUserNameTooLess4AndTooLong20(String username) {
+        //GIVEN
+        dto = new UserRequestDTO(1L, username, CORRECT_PASS, CORRECT_EMAIL);
+
+        //WHEN
+        errors = validator.validate(dto);
+
+        //THEN
+        assertTrue(errors.stream().anyMatch(e -> e.getMessage().equals("Username must be between 4 and 20 characters long")));
     }
 
 }
