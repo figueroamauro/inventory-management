67ad26d7e224c9d33fd2dcd8dec1a5b3636cdead
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-04-07 16:28:31 -0300

    feat(UserRequestDTO): Add validation for null and empty username

--- ms-users/src/main/java/ar/com/old/ms_users/dto/UserRequestDTO.java
+++ ms-users/src/main/java/ar/com/old/ms_users/dto/UserRequestDTO.java
@@ -1,4 +1,14 @@
 package ar.com.old.ms_users.dto;
 
-public record UserRequestDTO(Long id, String userName, String password, String email) {
+import jakarta.validation.constraints.NotBlank;
+
+public record UserRequestDTO(
+        Long id,
+
+        @NotBlank(message = "Username can not be blank")
+        String userName,
+
+        String password,
+
+        String email) {
 }
--- ms-users/src/test/java/ar/com/old/ms_users/dto/UserRequestDTOTest.java
+++ ms-users/src/test/java/ar/com/old/ms_users/dto/UserRequestDTOTest.java
@@ -3,7 +3,10 @@ package ar.com.old.ms_users.dto;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 
 import java.util.Set;
@@ -12,20 +15,41 @@ import static jakarta.validation.Validation.buildDefaultValidatorFactory;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserRequestDTOTest {
-    
-    @Test
-    void shouldCreateDto_whenAllFieldsAreValid(){
-        //GIVEN
-        ValidatorFactory validatorFactory = buildDefaultValidatorFactory();
-        Validator validator = validatorFactory.getValidator();
+    private Validator validator;
+    private UserRequestDTO dto;
+    private Set<ConstraintViolation<UserRequestDTO>> errors;
 
-        UserRequestDTO dto = new UserRequestDTO(1L, "test", "pass123", "test@mail.com");
+    @BeforeEach
+    void init() {
+        ValidatorFactory validatorFactory = buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+
+
+    }
+
+    @Test
+    void shouldCreateDto_whenAllFieldsAreValid() {
+        //GIVEN
+        dto = new UserRequestDTO(1L, "test", "pass123", "test@mail.com");
 
         //WHEN
-        Set<ConstraintViolation<UserRequestDTO>> errors = validator.validate(dto);
+        errors = validator.validate(dto);
 
         //THEN
         assertTrue(errors.isEmpty());
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void shouldThrowException_whenUserNameIsBlank(String username) {
+        //GIVEN
+        dto = new UserRequestDTO(1L, username, "pass123", "test@mail.com");
+
+        //WHEN
+        errors = validator.validate(dto);
+
+        //THEN
+        assertTrue(errors.stream().anyMatch(e -> e.getMessage().equals("Username can not be blank")));
     }
 
 }
