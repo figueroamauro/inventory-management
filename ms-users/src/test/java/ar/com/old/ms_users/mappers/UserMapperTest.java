153b1017426113b48beaf89e914a7438c0cfbef5
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-04-09 19:08:34 -0300

    feat(UserMapperTest): add test to validate mapping from User to UserResponseDTO

--- ms-users/src/test/java/ar/com/old/ms_users/mappers/UserMapperTest.java
+++ ms-users/src/test/java/ar/com/old/ms_users/mappers/UserMapperTest.java
@@ -1,29 +1,32 @@
 package ar.com.old.ms_users.mappers;
 
 import ar.com.old.ms_users.dto.UserRequestDTO;
+import ar.com.old.ms_users.dto.UserResponseDTO;
 import ar.com.old.ms_users.dto.UserUpdateRequestDTO;
 import ar.com.old.ms_users.entities.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserMapperTest {
 
     private final UserRequestMapper userRequestMapper = Mappers.getMapper(UserRequestMapper.class);
+    private final UserResponseMapper userResponseMapper = Mappers.getMapper(UserResponseMapper.class);
+    private User user;
+
+    @BeforeEach
+    void init() {
+        user = new User(1L, "test", "pass1234", "test@mail.com");
+    }
 
     @Nested
     class RequestMapper {
 
         @Test
         void shouldMapToDTO(){
-            //GIVEN
-            User user = new User(1L, "test", "pass1234", "test@mail.com");
-
             //WHEN
             UserRequestDTO result = userRequestMapper.toDto(user);
 
@@ -68,7 +71,22 @@ class UserMapperTest {
             assertEquals("newEmail@mail.com", result.getEmail());
         }
 
+    }
 
+    @Nested
+    class ResponseMapper {
+
+        @Test
+        void shouldMapToDto(){
+            //WHEN
+            UserResponseDTO result = userResponseMapper.toDto(user);
+
+            //THEN
+            assertNotNull(result);
+            assertEquals(1L, result.id());
+            assertEquals("test", result.userName());
+            assertEquals("test@mail.com",result.email());
+        }
     }
 
 }
