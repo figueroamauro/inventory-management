6557d3f43aabb39216958c17bea06542a98854ef
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-04-10 22:51:24 -0300

    feat(JwtService): add secret key to generate the token

--- ms-users/src/main/java/ar/com/old/ms_users/security/JwtService.java
+++ ms-users/src/main/java/ar/com/old/ms_users/security/JwtService.java
@@ -1,20 +1,24 @@
 package ar.com.old.ms_users.security;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Service
 public class JwtService {
-
+    private static final SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     private static final long EXPIRATION_TIME = 36000000;
 
     public String generateToken(CustomUserDetails userDetails) {
-         return Jwts.builder()
+        return Jwts.builder()
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SECRET_KEY)
                 .compact();
     }
 }
