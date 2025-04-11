e9c80234e506fced38c9bcb73315951f56d5872d
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-04-10 23:18:00 -0300

    feat(JwtService): add method to get username from the token

--- ms-users/src/main/java/ar/com/old/ms_users/security/JwtService.java
+++ ms-users/src/main/java/ar/com/old/ms_users/security/JwtService.java
@@ -1,5 +1,6 @@
 package ar.com.old.ms_users.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -13,12 +14,30 @@ public class JwtService {
     private static final SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     private static final long EXPIRATION_TIME = 36000000;
 
-    public String generateToken(CustomUserDetails userDetails) {
+    public String generateToken(CustomUserDetails userDetails, long expiration, SecretKey key) {
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SECRET_KEY)
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key)
                 .compact();
     }
+
+    public String generateToken(CustomUserDetails userDetails) {
+        return generateToken(userDetails, EXPIRATION_TIME, SECRET_KEY);
+    }
+
+    public String getSubject(String token, SecretKey key) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    public String getSubject(String token) {
+        return getSubject(token, SECRET_KEY);
+    }
+
 }
--- ms-users/src/test/java/ar/com/old/ms_users/security/JwtServiceTest.java
+++ ms-users/src/test/java/ar/com/old/ms_users/security/JwtServiceTest.java
@@ -1,16 +1,23 @@
 package ar.com.old.ms_users.security;
 
 import ar.com.old.ms_users.entities.User;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import javax.crypto.SecretKey;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class JwtServiceTest {
+
+    private static final SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final long EXPIRATION_TIME = 36000000;
 
     @Autowired
     private JwtService jwtService;
@@ -27,6 +34,21 @@ class JwtServiceTest {
         //THEN
         assertNotNull(token);
         assertTrue(token.length() > 50);
+    }
+
+    @Test
+    void shouldObtainUserName() {
+        //GIVEN
+        User user = new User(1L, "username", "pass1234", "test@mail.com");
+        CustomUserDetails userDetails = new CustomUserDetails(user);
+        String tokenMock = jwtService.generateToken(userDetails, EXPIRATION_TIME, SECRET_KEY);
+
+        //WHEN
+        String result = jwtService.getSubject(tokenMock, SECRET_KEY);
+
+        //THEN
+        assertNotNull(result);
+        assertEquals("username", result);
 
     }
 }
