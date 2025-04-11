e1fe5070fd50a4b089f1ec0d9066d951f7ab5ed0
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-04-10 22:48:45 -0300

    feat(JwtService): add method to generate Jwt.

--- ms-users/src/main/java/ar/com/old/ms_users/security/JwtService.java
+++ ms-users/src/main/java/ar/com/old/ms_users/security/JwtService.java
@@ -1,6 +1,20 @@
 package ar.com.old.ms_users.security;
 
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
 public class JwtService {
 
+    private static final long EXPIRATION_TIME = 36000000;
 
+    public String generateToken(CustomUserDetails userDetails) {
+         return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .compact();
+    }
 }
--- ms-users/src/test/java/ar/com/old/ms_users/security/JwtServiceTest.java
+++ ms-users/src/test/java/ar/com/old/ms_users/security/JwtServiceTest.java
@@ -3,10 +3,30 @@ package ar.com.old.ms_users.security;
 import ar.com.old.ms_users.entities.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@ActiveProfiles("test")
 class JwtServiceTest {
 
+    @Autowired
+    private JwtService jwtService;
 
+    @Test
+    void shouldGenerateToken() {
+        //GIVEN
+        User user = new User(1L, "test", "pass1234", "test@mail.com");
+        CustomUserDetails userDetails = new CustomUserDetails(user);
+
+        //WHEN
+        String token = jwtService.generateToken(userDetails);
+
+        //THEN
+        assertNotNull(token);
+        assertTrue(token.length() > 50);
+
+    }
 }
