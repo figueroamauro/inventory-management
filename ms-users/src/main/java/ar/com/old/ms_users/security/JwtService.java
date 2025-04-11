d77ffb5e0d04d8ac317149232085448dd0082c05
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-04-10 22:37:16 -0300

    chore(JwtService): add JWT dependencies

---
+++ ms-users/src/main/java/ar/com/old/ms_users/security/JwtService.java
@@ -0,0 +1,6 @@
+package ar.com.old.ms_users.security;
+
+public class JwtService {
+
+
+}
---
+++ ms-users/src/test/java/ar/com/old/ms_users/security/JwtServiceTest.java
@@ -0,0 +1,12 @@
+package ar.com.old.ms_users.security;
+
+import ar.com.old.ms_users.entities.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JwtServiceTest {
+
+
+}
