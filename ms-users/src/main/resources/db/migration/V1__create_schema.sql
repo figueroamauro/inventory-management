9164be0968f26f4e573dcb2d72fd23addaa177b4
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-04-14 15:43:54 -0300

    feat(Schema): add query to create user before starting application.

--- ms-users/src/main/resources/db/migration/V1__create_schema.sql
+++ ms-users/src/main/resources/db/migration/V1__create_schema.sql
@@ -9,3 +9,8 @@ email varchar(255) UNIQUE,
 enabled boolean,
 role varchar(20)
 );
+
+-- FOR TESTING
+
+INSERT INTO users(username, password, email,enabled,role)
+VALUES("admin", "$2y$10$jEVxb8Mh8y0ppp9XcX.9QO/9S2N9OBKXIdY9MAYGrxtXLpd5f8.dy", "admin@mail.com", true, "ADMIN");
