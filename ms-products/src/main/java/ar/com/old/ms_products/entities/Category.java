57f6419f5c83ce2501fc90999e863735f77fc6c6
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-04-15 23:28:36 -0300

    feat(Category): create entity Category

--- ms-products/src/main/java/ar/com/old/ms_products/entities/Category.java
+++ ms-products/src/main/java/ar/com/old/ms_products/entities/Category.java
@@ -1,5 +1,37 @@
 package ar.com.old.ms_products.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "category")
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
