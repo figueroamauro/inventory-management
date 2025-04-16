36f98ebaa330af6cacb9da778835174aa7a6dc14
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-04-15 23:39:39 -0300

    feat(Warehouse): create entity Warehouse

--- ms-products/src/main/java/ar/com/old/ms_products/entities/Warehouse.java
+++ ms-products/src/main/java/ar/com/old/ms_products/entities/Warehouse.java
@@ -1,4 +1,49 @@
 package ar.com.old.ms_products.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "warehouse")
 public class Warehouse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+
+    public Warehouse(Long id, String name, Long userId) {
+        this.id = id;
+        this.name = name;
+        this.userId = userId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 }
