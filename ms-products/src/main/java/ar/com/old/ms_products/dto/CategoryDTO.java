39cd4c109dde409fd67b906faae2a9131d029bb3
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-04-22 20:40:14 -0300

    feat(CategoryDTO): add validation for notBlank, size and pattern in name attribute

--- ms-products/src/main/java/ar/com/old/ms_products/dto/CategoryDTO.java
+++ ms-products/src/main/java/ar/com/old/ms_products/dto/CategoryDTO.java
@@ -1,4 +1,15 @@
 package ar.com.old.ms_products.dto;
 
-public record CategoryDTO(Long id, String name) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record CategoryDTO(
+        Long id,
+        @NotBlank(message = "Category can not be blank")
+        @Size(min = 4, max = 20, message = "Category must be between 4 and 20 characters long")
+        @Pattern(regexp = NAME_PATTERN,message = "Invalid category name pattern. Must contain only alphanumeric characters")
+        String name) {
+
+    private final static String NAME_PATTERN = "^[a-zA-Z]{4,}$";
 }
