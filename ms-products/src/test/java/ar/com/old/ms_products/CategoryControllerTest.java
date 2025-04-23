d96d3d8fcc213bec15c6cec3a0290b1e683beddd
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-04-22 23:39:01 -0300

    refactor(CategoryControllerTest): add init method with beforeEach annotation

--- ms-products/src/test/java/ar/com/old/ms_products/CategoryControllerTest.java
+++ ms-products/src/test/java/ar/com/old/ms_products/CategoryControllerTest.java
@@ -6,6 +6,9 @@ import ar.com.old.ms_products.entities.Warehouse;
 import ar.com.old.ms_products.exceptions.ExistingCategoryException;
 import ar.com.old.ms_products.services.CategoryService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jetbrains.annotations.NotNull;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,15 +37,20 @@ class CategoryControllerTest {
     @MockitoBean
     private CategoryService categoryService;
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private Category category;
+    private CategoryDTO dto;
+
+    @BeforeEach
+    void init() {
+        category = new Category(1L, "indumentaria", new Warehouse(1L, "warehouse", 1L));
+        dto = new CategoryDTO(null, "indumentaria");
+    }
+
 
     @Test
     void shouldFindAllCategories_status200() throws Exception {
         //GIVEN
-        List<Category> list = List.of(new Category(1L, "test", new Warehouse(1L, "warehouse", 1L)),
-                new Category(1L, "test", new Warehouse(2L, "warehouse", 1L)),
-                new Category(1L, "test", new Warehouse(3L, "warehouse", 1L)));
-        Pageable pageable = PageRequest.of(0, 10);
-        Page<Category> page = new PageImpl<>(list, pageable, list.size());
+        Page<Category> page = buildPage();
         when(categoryService.findAll(any(Pageable.class))).thenReturn(page);
 
         //WHEN
@@ -57,8 +65,7 @@ class CategoryControllerTest {
     @Test
     void shouldCreateCategory_status201() throws Exception {
         //GIVEN
-        CategoryDTO dto = new CategoryDTO(null, "indumentaria");
-        when(categoryService.create(dto)).thenReturn(new Category(1L, "indumentaria", new Warehouse(1L, "deposito", 1L)));
+        when(categoryService.create(dto)).thenReturn(category);
 
         //WHEN
         mockMvc.perform(post("/api/categories")
@@ -75,7 +82,6 @@ class CategoryControllerTest {
     @Test
     void shouldFailCreatingCategory_status409() throws Exception {
         //GIVEN
-        CategoryDTO dto = new CategoryDTO(null, "indumentaria");
         when(categoryService.create(dto)).thenThrow(new ExistingCategoryException("Category already exist"));
 
         //WHEN
@@ -87,6 +93,11 @@ class CategoryControllerTest {
                 .andExpect(status().isConflict())
                 .andExpect(jsonPath("$").isMap())
                 .andExpect(jsonPath("$.error").value("Category already exist"));
+    }
 
+    private @NotNull Page<Category> buildPage() {
+        List<Category> list = List.of(category,category,category);
+        Pageable pageable = PageRequest.of(0, 10);
+        return new PageImpl<>(list, pageable, list.size());
     }
 }
