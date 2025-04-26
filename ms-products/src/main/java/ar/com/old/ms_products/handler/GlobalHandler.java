cf965a1bbb43c9bd5b4da69193dcf0b839c1d561
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-04-26 16:21:43 -0300

    test(WarehouseControllerTest): add test for existing warehouse in create method

--- ms-products/src/main/java/ar/com/old/ms_products/handler/GlobalHandler.java
+++ ms-products/src/main/java/ar/com/old/ms_products/handler/GlobalHandler.java
@@ -3,6 +3,7 @@ package ar.com.old.ms_products.handler;
 import ar.com.old.ms_products.exceptions.CategoryNotFoundException;
 import ar.com.old.ms_products.exceptions.ConnectionFeignException;
 import ar.com.old.ms_products.exceptions.ExistingCategoryException;
+import ar.com.old.ms_products.exceptions.WarehouseAlreadyExistException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -18,7 +19,9 @@ import java.util.stream.Collectors;
 @RestControllerAdvice
 public class GlobalHandler {
 
-    @ExceptionHandler(ExistingCategoryException.class)
+    @ExceptionHandler({
+            ExistingCategoryException.class,
+            WarehouseAlreadyExistException.class})
     public ResponseEntity<Map<String, String>> handlerConflict(Exception e) {
         return buildResponseError(e, HttpStatus.CONFLICT);
     }
--- ms-products/src/test/java/ar/com/old/ms_products/controllers/WarehouseControllerTest.java
+++ ms-products/src/test/java/ar/com/old/ms_products/controllers/WarehouseControllerTest.java
@@ -2,8 +2,11 @@ package ar.com.old.ms_products.controllers;
 
 import ar.com.old.ms_products.dto.WarehouseDTO;
 import ar.com.old.ms_products.entities.Warehouse;
+import ar.com.old.ms_products.exceptions.WarehouseAlreadyExistException;
 import ar.com.old.ms_products.services.WarehouseServiceImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,12 +28,19 @@ class WarehouseControllerTest {
     @MockitoBean
     private WarehouseServiceImpl warehouseService;
     private final ObjectMapper objectMapper = new ObjectMapper();
+    WarehouseDTO dto;
+    Warehouse warehouse;
+
+    @BeforeEach
+    void init() {
+        dto = new WarehouseDTO(1L, "deposito");
+        warehouse = new Warehouse(1L, "deposito", 1L);
+    }
 
     @Test
     void shouldCreateWarehouse_status201() throws Exception {
         //GIVEN
-        WarehouseDTO dto = new WarehouseDTO(1L, "deposito");
-        when(warehouseService.create(dto)).thenReturn(new Warehouse(1L, "deposito", 1L));
+        when(warehouseService.create(dto)).thenReturn(warehouse);
 
         //WHEN
         mockMvc.perform(post("/api/warehouses")
@@ -41,5 +51,21 @@ class WarehouseControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$").isMap())
                 .andExpect(jsonPath("$.name").value("deposito"));
+    }
+
+    @Test
+    void shouldFailCreatingWarehouse_whenAlreadyExist_status409() throws Exception {
+        //GIVEN
+        when(warehouseService.create(dto)).thenThrow(new WarehouseAlreadyExistException("Warehouse already exist"));
+
+        //WHEN
+        mockMvc.perform(post("/api/warehouses")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto)))
+
+                //THEN
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$").isMap())
+                .andExpect(jsonPath("$.error").value("Warehouse already exist"));
     }
 }
