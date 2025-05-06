4395ec54436905620ac5565c9fabe263f185708e
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-05-05 22:46:06 -0300

    test(LocationRepositoryTest): add test for finding all location by warehouse id

--- ms-stock/src/test/java/ar/com/old/ms_stock/repositories/LocationRepositoryTest.java
+++ ms-stock/src/test/java/ar/com/old/ms_stock/repositories/LocationRepositoryTest.java
@@ -9,6 +9,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
@@ -34,5 +37,20 @@ class LocationRepositoryTest {
         //THEN
         assertTrue(result.isPresent());
         assertEquals("B1", result.get().getName());
+    }
+
+    @Test
+    void shouldFindAllByWarehouseId(){
+        //GIVEN
+        Pageable pageable = PageRequest.of(0, 10);
+
+        //WHEN
+        Page<Location> result = locationRepository.findAllByWarehouseId(pageable, 1L);
+
+        //THEN
+        assertNotNull(result);
+        assertEquals(2, result.getTotalElements());
+        assertTrue(result.stream().anyMatch(location -> location.getName().equals("B1")));
+        assertTrue(result.stream().anyMatch(location -> location.getName().equals("B2")));
     }
 }
