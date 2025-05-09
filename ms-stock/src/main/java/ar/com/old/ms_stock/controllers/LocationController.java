fffb4f6022e8483bbafb7e06f6d75d7a0983f2c8
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-05-08 23:34:29 -0300

    feat(LocationController): implement findAll method

--- ms-stock/src/main/java/ar/com/old/ms_stock/controllers/LocationController.java
+++ ms-stock/src/main/java/ar/com/old/ms_stock/controllers/LocationController.java
@@ -3,11 +3,11 @@ package ar.com.old.ms_stock.controllers;
 import ar.com.old.ms_stock.dto.LocationDTO;
 import ar.com.old.ms_stock.entities.Location;
 import ar.com.old.ms_stock.services.LocationService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -26,5 +26,12 @@ public class LocationController {
         Location location = locationService.create(dto);
 
         return ResponseEntity.created(URI.create("api/locations/" + location.getId())).body(location);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findAll(Pageable pageable, PagedResourcesAssembler<Location> assembler) {
+        Page<Location> page = locationService.findAll(pageable);
+
+        return ResponseEntity.ok(assembler.toModel(page));
     }
 }
--- ms-stock/src/test/java/ar/com/old/ms_stock/controllers/LocationControllerTest.java
+++ ms-stock/src/test/java/ar/com/old/ms_stock/controllers/LocationControllerTest.java
@@ -5,14 +5,21 @@ import ar.com.old.ms_stock.entities.Location;
 import ar.com.old.ms_stock.exceptions.LocationAlreadyExistException;
 import ar.com.old.ms_stock.services.LocationServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -26,13 +33,20 @@ class LocationControllerTest {
     private MockMvc mockMvc;
     @MockitoBean
     private LocationServiceImpl locationService;
+
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private LocationDTO dto;
+    private Location location;
+
+    @BeforeEach
+    void init() {
+        dto = new LocationDTO(1L, "B2");
+        location = new Location(1L, "B2", 1L);
+    }
 
     @Test
     void shouldCreateLocation_return201() throws Exception {
         //GIVEN
-        LocationDTO dto = new LocationDTO(1L, "B2");
-        Location location = new Location(1L, "B2", 1L);
         when(locationService.create(dto)).thenReturn(location);
 
         //WHEN
@@ -49,7 +63,6 @@ class LocationControllerTest {
     @Test
     void shouldFailCreatingLocation_whenAlreadyExist_return409() throws Exception {
         //GIVEN
-        LocationDTO dto = new LocationDTO(1L, "B2");
         when(locationService.create(dto)).thenThrow(new LocationAlreadyExistException("Location already exist"));
 
         //WHEN
@@ -61,5 +74,22 @@ class LocationControllerTest {
                 .andExpect(status().isConflict())
                 .andExpect(jsonPath("$").isMap())
                 .andExpect(jsonPath("$.error").value("Location already exist"));
+    }
+
+    @Test
+    void shouldFindAllLocations_return200() throws Exception {
+        //GIVEN
+        List<Location> list = List.of(location, location, location);
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Location> page = new PageImpl<>(list, pageable, list.size());
+        when(locationService.findAll(any(Pageable.class))).thenReturn(page);
+
+        //WHEN
+        mockMvc.perform(get("/api/locations")
+                        .contentType(MediaType.APPLICATION_JSON))
+
+                //THEN
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isMap());
     }
 }
