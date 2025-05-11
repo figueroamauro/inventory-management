1535df12cc89477716eafc8e1edfc3a52fc0c65a
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-05-11 19:02:07 -0300

    feat(StockMovementService): implement create method

--- ms-stock/src/main/java/ar/com/old/ms_stock/services/StockMovementServiceImpl.java
+++ ms-stock/src/main/java/ar/com/old/ms_stock/services/StockMovementServiceImpl.java
@@ -3,30 +3,40 @@ package ar.com.old.ms_stock.services;
 import ar.com.old.ms_stock.clients.WarehouseClientService;
 import ar.com.old.ms_stock.clients.dto.WarehouseDTO;
 import ar.com.old.ms_stock.dto.StockMovementDTO;
+import ar.com.old.ms_stock.entities.Location;
 import ar.com.old.ms_stock.entities.StockEntry;
 import ar.com.old.ms_stock.entities.StockMovement;
+import ar.com.old.ms_stock.repositories.LocationRepository;
 import ar.com.old.ms_stock.repositories.StockEntryRepository;
 import ar.com.old.ms_stock.repositories.StockMovementRepository;
 import org.springframework.data.domain.Page;
+
+import java.util.Optional;
 
 public class StockMovementServiceImpl implements StockMovementService{
     private final StockMovementRepository stockMovementRepository;
     private final StockEntryRepository stockEntryRepository;
     private final WarehouseClientService clientService;
+    private final LocationRepository locationRepository;
 
-    public StockMovementServiceImpl(StockMovementRepository stockMovementRepository, StockEntryRepository stockEntryRepository, WarehouseClientService clientService) {
+    public StockMovementServiceImpl(StockMovementRepository stockMovementRepository, StockEntryRepository stockEntryRepository, WarehouseClientService clientService, LocationRepository locationRepository) {
         this.stockMovementRepository = stockMovementRepository;
         this.stockEntryRepository = stockEntryRepository;
         this.clientService = clientService;
+        this.locationRepository = locationRepository;
     }
 
     @Override
     public StockMovement create(StockMovementDTO dto) {
         WarehouseDTO warehouse = clientService.getWarehouse();
         StockEntry entry = stockEntryRepository.findByIdAndWarehouseId(dto.productId(), warehouse.id())
-                .orElseGet(()-> stockEntryRepository.save(new StockEntry(dto.quantity(),dto.productId(),warehouse.id())));
+                .orElseGet(() -> stockEntryRepository.save(new StockEntry(dto.quantity(), dto.productId(), warehouse.id())));
 
-        return null;
+        Location location = locationRepository.findByIdAndWarehouseId(dto.locationId(), warehouse.id())
+                .orElseThrow();
+
+        StockMovement stockMovement = new StockMovement(null, dto.type(), dto.quantity(), dto.note(), location, entry);
+        return stockMovementRepository.save(stockMovement);
     }
 
     @Override
--- ms-stock/src/test/java/ar/com/old/ms_stock/services/StockMovementServiceTest.java
+++ ms-stock/src/test/java/ar/com/old/ms_stock/services/StockMovementServiceTest.java
@@ -3,25 +3,31 @@ package ar.com.old.ms_stock.services;
 import ar.com.old.ms_stock.clients.WarehouseClientService;
 import ar.com.old.ms_stock.clients.dto.WarehouseDTO;
 import ar.com.old.ms_stock.dto.StockMovementDTO;
+import ar.com.old.ms_stock.entities.Location;
 import ar.com.old.ms_stock.entities.StockEntry;
 import ar.com.old.ms_stock.entities.StockMovement;
 import ar.com.old.ms_stock.enums.MovementType;
+import ar.com.old.ms_stock.repositories.LocationRepository;
 import ar.com.old.ms_stock.repositories.StockEntryRepository;
 import ar.com.old.ms_stock.repositories.StockMovementRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+
 import static org.mockito.Mockito.*;
+
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 class StockMovementServiceTest {
-
     @InjectMocks
     private StockMovementServiceImpl stockMovementService;
     @Mock
@@ -30,21 +36,57 @@ class StockMovementServiceTest {
     private StockMovementRepository stockMovementRepository;
     @Mock
     private WarehouseClientService clientService;
+    @Mock
+    private LocationRepository locationRepository;
+
+    private WarehouseDTO warehouseDTO;
+    private StockEntry stockEntry;
+    private StockMovementDTO dto;
+    private StockMovement stockMovement;
+    private Location location;
+
+    @BeforeEach
+    void init() {
+        warehouseDTO = new WarehouseDTO(1L, "warehouse", 1L);
+        stockEntry = new StockEntry(20, 1L, 1L);
+        dto = new StockMovementDTO(MovementType.IN, 20, "", 1L, 1L);
+        location = new Location(1L, "B1", 1L);
+        stockMovement = new StockMovement(1L, MovementType.IN, 20, "", location, stockEntry);
+    }
 
     @Test
-    void shouldCreateStockEntryBeforeToCreateFirstMovement(){
+    void shouldCreateStockEntryBeforeToCreateFirstMovement() {
         //GIVEN
-        WarehouseDTO warehouseDTO = new WarehouseDTO(1L, "warehouse", 1L);
-        StockEntry stockEntry = new StockEntry(20, 1L, 1L);
-        StockMovementDTO dto = new StockMovementDTO(MovementType.IN, 20, "", 1L, 1L);
         when(clientService.getWarehouse()).thenReturn(warehouseDTO);
         when(stockEntryRepository.save(any(StockEntry.class))).thenReturn(stockEntry);
+        when(locationRepository.findByIdAndWarehouseId(1L, 1L)).thenReturn(Optional.of(new Location(1L, "B1", 1L)));
 
         //WHEN
         stockMovementService.create(dto);
 
         //THEN
-        verify(stockEntryRepository).findByIdAndWarehouseId(1L,1L);
+        verify(stockEntryRepository).findByIdAndWarehouseId(1L, 1L);
+        verify(stockEntryRepository).save(any(StockEntry.class));
+    }
+
+    @Test
+    void shouldCreateStockMovement() {
+        //GIVEN
+        when(clientService.getWarehouse()).thenReturn(warehouseDTO);
+        when(stockEntryRepository.save(any(StockEntry.class))).thenReturn(stockEntry);
+        when(locationRepository.findByIdAndWarehouseId(1L, 1L)).thenReturn(Optional.of(location));
+        when(stockMovementRepository.save(any(StockMovement.class))).thenReturn(stockMovement);
+
+        //WHEN
+        StockMovement result = stockMovementService.create(dto);
+
+        //THEN
+        assertNotNull(result);
+        assertEquals(20, result.getQuantity());
+        assertEquals(1L,result.getStockEntry().getId());
+
+        verify(stockEntryRepository).findByIdAndWarehouseId(1L, 1L);
+        verify(locationRepository).findByIdAndWarehouseId(1L, 1L);
         verify(stockEntryRepository).save(any(StockEntry.class));
     }
 }
