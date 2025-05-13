b5a9db48debe2c5eb4011054bdace5f2854b4e6d
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-05-13 20:04:02 -0300

    feat(StockMovementDTO): add validations for all params

--- ms-stock/src/main/java/ar/com/old/ms_stock/dto/StockMovementDTO.java
+++ ms-stock/src/main/java/ar/com/old/ms_stock/dto/StockMovementDTO.java
@@ -1,12 +1,23 @@
 package ar.com.old.ms_stock.dto;
 
 import ar.com.old.ms_stock.enums.MovementType;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record StockMovementDTO(
+        @Pattern(regexp = "^[A-Z_]+$", message = "Invalid type. Only uppercase letters and underscores are allowed")
+        @Size(min = 2, max = 15, message = "Type must contain between 2 and 15 characters")
+        @NotNull
         MovementType type,
+        @NotNull(message = "Quantity can not be null")
         Integer quantity,
+        @Max(value = 255)
         String note,
+        @NotNull(message = "Location id can not be null")
         Long locationId,
+        @NotNull(message = "Product id can not be null")
         Long productId
 
 ) {
