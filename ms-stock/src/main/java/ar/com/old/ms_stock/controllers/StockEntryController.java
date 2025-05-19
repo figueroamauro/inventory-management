5264544fc8fb3e8c5781a8f5bb797491fd7f7563
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-05-19 16:50:37 -0300

    feat(StockEntryController): implement findAll method

---
+++ ms-stock/src/main/java/ar/com/old/ms_stock/controllers/StockEntryController.java
@@ -0,0 +1,29 @@
+package ar.com.old.ms_stock.controllers;
+
+import ar.com.old.ms_stock.entities.StockEntry;
+import ar.com.old.ms_stock.services.StockEntryService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/stock")
+public class StockEntryController {
+    private final StockEntryService stockEntryService;
+
+    public StockEntryController(StockEntryService stockEntryService) {
+        this.stockEntryService = stockEntryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findAll(Pageable pageable, PagedResourcesAssembler<StockEntry> assembler) {
+        Page<StockEntry> page = stockEntryService.findAll(pageable);
+
+        return ResponseEntity.ok(assembler.toModel(page));
+    }
+
+}
---
+++ ms-stock/src/test/java/ar/com/old/ms_stock/controllers/StockEntryControllerTest.java
@@ -0,0 +1,59 @@
+package ar.com.old.ms_stock.controllers;
+
+import ar.com.old.ms_stock.entities.StockEntry;
+import ar.com.old.ms_stock.services.StockEntryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(StockEntryController.class)
+@ActiveProfiles("test")
+class StockEntryControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private StockEntryService stockEntryService;
+
+    private StockEntry stockEntry;
+
+    @BeforeEach
+    void init() {
+        stockEntry = new StockEntry(100, 1L, 1L);
+    }
+
+
+    @Test
+    void shouldFindAll_status200() throws Exception {
+        //GIVEN
+        List<StockEntry> list = List.of(stockEntry, stockEntry, stockEntry);
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<StockEntry> page = new PageImpl<>(list, pageable, list.size());
+        when(stockEntryService.findAll(any(Pageable.class))).thenReturn(page);
+
+        //WHEN
+        mockMvc.perform(get("/api/stock")
+                        .contentType(MediaType.APPLICATION_JSON))
+
+                //THEN
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isMap());
+    }
+}
