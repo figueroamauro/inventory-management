48306d9df925f564e64d49c87568e22ccb972b38
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-06-05 23:28:37 -0300

    feat(CategoryController): add swagger annotations in all endpoints

--- ms-products/src/main/java/ar/com/old/ms_products/controllers/CategoryController.java
+++ ms-products/src/main/java/ar/com/old/ms_products/controllers/CategoryController.java
@@ -3,6 +3,10 @@ package ar.com.old.ms_products.controllers;
 import ar.com.old.ms_products.dto.CategoryDTO;
 import ar.com.old.ms_products.entities.Category;
 import ar.com.old.ms_products.services.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,9 +17,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
+@Tag(name = "Categorias", description = "Gestión de categorias: creación, eliminación y consulta de información")
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
+    private static final String DEFAULT_PAGE = "{\"page\": 0, \"size\": 10, \"sort\": \"name\"}";
 
     private final CategoryService categoryService;
 
@@ -24,6 +30,8 @@ public class CategoryController {
     }
 
 
+    @ApiResponse(responseCode = "201")
+    @Operation(summary = "crear categoria")
     @PostMapping
     public ResponseEntity<Category> create(@Valid @RequestBody CategoryDTO categoryDTO) {
         Category category = categoryService.create(categoryDTO);
@@ -31,19 +39,28 @@ public class CategoryController {
         return ResponseEntity.created(URI.create("/api/categories/" + category.getId())).body(category);
     }
 
+
+    @ApiResponse(responseCode = "200")
+    @Operation(summary = "Obtener lista de categorias")
     @GetMapping
-    public ResponseEntity<?> findAll(@PageableDefault(sort = "name") Pageable pageable, PagedResourcesAssembler<Category> assembler) {
+    public ResponseEntity<?> findAll(@PageableDefault(sort = "name")
+                                         @Schema(example = DEFAULT_PAGE)
+                                         Pageable pageable, PagedResourcesAssembler<Category> assembler) {
         Page<Category> page = categoryService.findAll(pageable);
 
         return ResponseEntity.ok(assembler.toModel(page));
     }
 
+    @ApiResponse(responseCode = "200")
+    @Operation(summary = "Obtener categoria por id")
     @GetMapping("/{id}")
     public ResponseEntity<Category> findOne(@PathVariable Long id) {
         Category category = categoryService.findOne(id);
         return ResponseEntity.ok(category);
     }
 
+    @ApiResponse(responseCode = "204")
+    @Operation(summary = "Eliminar una categoria por su id")
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         categoryService.delete(id);
--- ms-products/src/main/java/ar/com/old/ms_products/dto/CategoryDTO.java
+++ ms-products/src/main/java/ar/com/old/ms_products/dto/CategoryDTO.java
@@ -1,11 +1,15 @@
 package ar.com.old.ms_products.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record CategoryDTO(
+        @Schema(name = "1")
         Long id,
+
+        @Schema(name = "Indumentaria")
         @NotBlank(message = "Category can not be blank")
         @Size(min = 4, max = 20, message = "Category must be between 4 and 20 characters long")
         @Pattern(regexp = NAME_PATTERN,message = "Invalid category name pattern. Must contain only alphanumeric characters")
