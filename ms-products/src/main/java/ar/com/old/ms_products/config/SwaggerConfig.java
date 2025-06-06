22890141c67607b2396d66e1dfb4748a5f953248
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-06-05 22:42:05 -0300

    feat(SwaggerConfig): add SwaggerConfig and its dependencies

---
+++ ms-products/src/main/java/ar/com/old/ms_products/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package ar.com.old.ms_products.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Bonpland API")
+                        .version("1.0")
+                        .description("Documentación de la API de Bonpland"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .name("bearerAuth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
