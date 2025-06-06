45a0bfff69b7b8fa6e9d0df64f75a644d0c2d32a
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-06-06 08:49:38 -0300

    feat(StockEntryController): add swagger annotations in all endpoints

--- ms-stock/src/main/java/ar/com/old/ms_stock/controllers/StockEntryController.java
+++ ms-stock/src/main/java/ar/com/old/ms_stock/controllers/StockEntryController.java
@@ -2,6 +2,10 @@ package ar.com.old.ms_stock.controllers;
 
 import ar.com.old.ms_stock.entities.StockEntry;
 import ar.com.old.ms_stock.services.StockEntryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
@@ -11,22 +15,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Stock", description = "Gestión de stock: consulta de información")
 @RestController
 @RequestMapping("/api/stock")
 public class StockEntryController {
+    private static final String DEFAULT_PAGE = "{\"page\": 0, \"size\": 10, \"sort\": \"name\"}";
     private final StockEntryService stockEntryService;
 
     public StockEntryController(StockEntryService stockEntryService) {
         this.stockEntryService = stockEntryService;
     }
 
+    @ApiResponse(responseCode = "200")
+    @Operation(summary = "Obtener lista de stock")
     @GetMapping
-    public ResponseEntity<?> findAll(Pageable pageable, PagedResourcesAssembler<StockEntry> assembler) {
+    public ResponseEntity<?> findAll(@Schema(example = DEFAULT_PAGE) Pageable pageable, PagedResourcesAssembler<StockEntry> assembler) {
         Page<StockEntry> page = stockEntryService.findAll(pageable);
 
         return ResponseEntity.ok(assembler.toModel(page));
     }
 
+    @ApiResponse(responseCode = "200")
+    @Operation(summary = "Obtener stock por su id")
     @GetMapping("/{id}")
     public ResponseEntity<StockEntry> findOne(@PathVariable Long id) {
         StockEntry stockEntry = stockEntryService.findOne(id);
