21ee8075958b49e8f61d767c27f83523aee4018c
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-06-05 23:39:35 -0300

    feat(ProductController): add swagger annotations in all endpoints

--- ms-products/src/main/java/ar/com/old/ms_products/controllers/ProductController.java
+++ ms-products/src/main/java/ar/com/old/ms_products/controllers/ProductController.java
@@ -5,6 +5,10 @@ import ar.com.old.ms_products.dto.ProductResponseDTO;
 import ar.com.old.ms_products.dto.ProductUpdateDTO;
 import ar.com.old.ms_products.entities.Product;
 import ar.com.old.ms_products.services.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,9 +19,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
+@Tag(name = "Productos", description = "Gestión de productos: creación, actualización, eliminación y consulta de información")
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
+    private static final String DEFAULT_PAGE = "{\"page\": 0, \"size\": 10, \"sort\": \"name\"}";
 
     private final ProductService productService;
 
@@ -25,6 +31,8 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @ApiResponse(responseCode = "201")
+    @Operation(summary = "Crear productos")
     @PostMapping
     public ResponseEntity<ProductResponseDTO> create(@Valid @RequestBody ProductDTO dto) {
         Product product = productService.create(dto);
@@ -33,8 +41,10 @@ public class ProductController {
         return ResponseEntity.created(URI.create("/api/products/" + product.getId())).body(responseDTO);
     }
 
+    @ApiResponse(responseCode = "200")
+    @Operation(summary = "Obtener lista de productos")
     @GetMapping
-    public ResponseEntity<?> findAll(@PageableDefault Pageable pageable,
+    public ResponseEntity<?> findAll(@PageableDefault   @Schema(example = DEFAULT_PAGE) Pageable pageable,
                                      PagedResourcesAssembler<ProductResponseDTO> assembler) {
 
         Page<Product> page = productService.findAll(pageable);
@@ -43,6 +53,8 @@ public class ProductController {
         return ResponseEntity.ok(assembler.toModel(result));
     }
 
+    @ApiResponse(responseCode = "200")
+    @Operation(summary = "Obtener producto por su id")
     @GetMapping("/{id}")
     public ResponseEntity<ProductResponseDTO> findOne(@PathVariable Long id) {
         Product product = productService.findOne(id);
@@ -51,6 +63,8 @@ public class ProductController {
         return ResponseEntity.ok(response);
     }
 
+    @ApiResponse(responseCode = "200")
+    @Operation(summary = "Actualizar producto")
     @PutMapping
     public ResponseEntity<ProductResponseDTO> update(@Valid @RequestBody ProductUpdateDTO dto) {
         Product product = productService.update(dto);
@@ -59,6 +73,8 @@ public class ProductController {
         return ResponseEntity.ok(response);
     }
 
+    @ApiResponse(responseCode = "200")
+    @Operation(summary = "Eliminar producto por su id")
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         productService.delete(id);
--- ms-products/src/main/java/ar/com/old/ms_products/dto/ProductDTO.java
+++ ms-products/src/main/java/ar/com/old/ms_products/dto/ProductDTO.java
@@ -1,22 +1,27 @@
 package ar.com.old.ms_products.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 
 public record ProductDTO(
+        @Schema(name = "Campera negra lisa")
         @NotBlank(message = "Product can not be blank")
         @Size(min = 4, max = 30, message = "Product must be between 4 and 30 characters long")
         @Pattern(regexp = NAME_PATTERN, message = "Invalid product name pattern. Must contain only alphanumeric characters")
         String name,
 
+        @Schema(name = "Campera negra lisa de algodón")
         @NotBlank(message = "Description can not be blank")
         @Size(min = 4, max = 200, message = "Product must be between 4 and 200 characters long")
         String description,
 
+        @Schema(name = "100.0")
         @Min(value = 0, message = "Price must be longer than 0")
         @Max(value = 50000000, message = "Price must be less than 50.000.000")
         @NotNull(message = "Price can not be null")
         Double price,
 
+        @Schema(name = "1")
         @NotNull(message = "Category id can not be null")
         Long categoryId
 ) {
